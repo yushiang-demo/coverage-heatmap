688a461374d1e3067b56af43fbabbe7ca0b7c5d8
yushiang <tsengyushiang@gmail.com>
2024-05-26 19:01:29 +0800

    doc: roomGeometry and app.

---
+++ packages/three-coverage-heatmap/src/Geometry/RoomBufferGeometry/index.js
@@ -0,0 +1,111 @@
+import * as THREE from "three";
+
+/**
+ * Represents a room with control over its features like walls, doors, furniture and signals.
+ * @class
+ * @extends THREE.BufferGeometry
+ */
+class RoomBufferGeometry extends THREE.BufferGeometry {
+  constructor() {
+    super();
+    this.aabbArray = [];
+    this.planeArray = [];
+    this.floorVertices = [];
+  }
+
+  _updateGeometry() {
+    const aabbVertices = this.aabbArray.flatMap(([min, max]) => {
+      const width = max[0] - min[0];
+      const height = max[1] - min[1];
+      const depth = max[2] - min[2];
+
+      const aabbGeometry = new THREE.BoxGeometry(width, height, depth);
+      aabbGeometry.translate(
+        (max[0] + min[0]) / 2,
+        (max[1] + min[1]) / 2,
+        (max[2] + min[2]) / 2
+      );
+
+      const vertices = [...aabbGeometry.attributes.position.array];
+      const index = [...aabbGeometry.getIndex().array];
+      const data = index.flatMap((index) => [
+        vertices[index * 3],
+        vertices[index * 3 + 1],
+        vertices[index * 3 + 2],
+      ]);
+      return data;
+    });
+    const planeVertices = this.planeArray.flatMap(([min, max]) => {
+      const vertexVectors = [
+        new THREE.Vector3(min[0], min[1], min[2]),
+        new THREE.Vector3(max[0], min[1], max[2]),
+        new THREE.Vector3(max[0], max[1], max[2]),
+        new THREE.Vector3(min[0], max[1], min[2]),
+        new THREE.Vector3(min[0], min[1], min[2]),
+        new THREE.Vector3(max[0], max[1], max[2]),
+      ];
+
+      return vertexVectors.flatMap((vec) => vec.toArray());
+    });
+
+    this.setAttribute(
+      "position",
+      new THREE.BufferAttribute(
+        new Float32Array([
+          ...this.floorVertices.flat(),
+          ...aabbVertices,
+          ...planeVertices,
+        ]),
+        3
+      )
+    );
+  }
+
+  /**
+   * Sets the floor size.
+   * @param {number} width The width of the room.
+   * @param {number} length The length of the room.
+   * @example
+   * roomBufferGeometry.setFloor(20, 20);
+   */
+  setFloor(width, length) {
+    this.floorVertices = [
+      [1, 0, 1],
+      [-1, 0, 1],
+      [1, 0, -1],
+      [1, 0, -1],
+      [-1, 0, 1],
+      [-1, 0, -1],
+    ].flatMap(([x, y, z]) => [(x * width) / 2, y, (z * length) / 2]);
+  }
+
+  /**
+   * Sets the Axis-Aligned Bounding Box (AABB) data.
+   * @param {Array<Vector.Vector3Pair>} data An array containing two elements, each representing a start and end vector, both three-dimensional.
+   * @example
+   * roomBufferGeometry.setAABB([
+   *   [[0, 0, 0], [1, 1, 1]]
+   * ]);
+   */
+  setAABB(data) {
+    if (!data) return;
+    this.aabbArray = data;
+    this._updateGeometry();
+  }
+
+  /**
+   * Sets the plane data.
+   * @param {Array<Vector.Vector3Pair>} data An array containing two elements, each representing a start and end vector, both three-dimensional.
+   * @example
+   * roomBufferGeometry.setPlane([
+   *   [[0, 0, 0], [1, 1, 1]]
+   * ]);
+   */
+  setPlane(data) {
+    if (!data) return;
+    this.planeArray = data;
+    this._updateGeometry();
+  }
+}
+
+export default RoomBufferGeometry;
--- packages/three-coverage-heatmap/src/jsdoc/types.js
+++ packages/three-coverage-heatmap/src/jsdoc/types.js
@@ -15,3 +15,38 @@
  * @memberof THREE
  * @see {@link https://threejs.org/docs/?q=Texture#api/en/textures/Texture}
  */
+
+/**
+ * @typedef BufferGeometry
+ * @memberof THREE
+ * @see {@link https://threejs.org/docs/?q=BufferGeometry#api/en/core/BufferGeometry}
+ */
+
+/**
+ * @typedef Material
+ * @memberof THREE
+ * @see {@link https://threejs.org/docs/?q=Mater#api/en/materials/Material}
+ */
+
+/**
+ * @typedef Vector3
+ * @memberof THREE
+ * @see {@link https://threejs.org/docs/?q=Vec#api/en/math/Vector3}
+ */
+
+/**
+ * A namespace defined for fixed-size array.
+ * @namespace Vector
+ */
+
+/**
+ * Represents a three-dimensional coordinate [x, y, z].
+ * @typedef {Array<number>} Vector3
+ * @memberof Vector
+ */
+
+/**
+ * Represents a coordinate pair [[x1, y1, z1], [x2, y2, z2]].
+ * @typedef {Array<Vector3>} Vector3Pair
+ * @memberof Vector
+ */
