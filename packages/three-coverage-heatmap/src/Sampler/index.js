375860030303b3f1b0e38417aa9e10ac991ce14d
tsengyushiang <tsengyushiang@gmail.com>
2024-08-10 06:14:18 +0000

    feature: convert texture to vertex color of sampler.

--- packages/three-coverage-heatmap/src/Sampler/index.js
+++ packages/three-coverage-heatmap/src/Sampler/index.js
@@ -1,7 +1,13 @@
 import * as THREE from "three";
 import HeatmapTextureMaterial from "../Material/HeatmapTextureMaterial";
 
-class Sampler extends THREE.Mesh {
+const LAYER_COUNT = 3 ** 2;
+const TEXTURE_ALTAS_SIZE = Math.ceil(Math.sqrt(LAYER_COUNT));
+const SAMPLES_PER_AXIS = 50;
+const SAMPLES_SCALE = [20, 3, 20];
+const SAMPLES_OFFSET = [-10, 0, -10];
+
+class Sampler extends THREE.Points {
   constructor() {
     const heatmapPlanes = (() => {
       const geometry = new THREE.BufferGeometry();
@@ -31,14 +37,13 @@ class Sampler extends THREE.Mesh {
             data.map((v, index) => v * scale[index] + offset[index])
           );
 
-        const textureUnit = Math.ceil(Math.sqrt(layers));
-        const layerSize = 1 / textureUnit;
+        const layerSize = 1 / TEXTURE_ALTAS_SIZE;
         const uvs = coords.flatMap((data, index) => {
-          const xOffset = index % textureUnit;
-          const yOffset = Math.floor(index / textureUnit);
+          const xOffset = index % TEXTURE_ALTAS_SIZE;
+          const yOffset = Math.floor(index / TEXTURE_ALTAS_SIZE);
           const uv = data.flatMap(([x, _, z]) => [
-            x / textureUnit + xOffset * layerSize,
-            z / textureUnit + yOffset * layerSize,
+            x / TEXTURE_ALTAS_SIZE + xOffset * layerSize,
+            z / TEXTURE_ALTAS_SIZE + yOffset * layerSize,
           ]);
           return uv;
         });
@@ -47,7 +52,7 @@ class Sampler extends THREE.Mesh {
           vertices,
           uvs,
         };
-      })(2 ** 2, [20, 3, 20], [-10, 0, -10]);
+      })(LAYER_COUNT, SAMPLES_SCALE, SAMPLES_OFFSET);
 
       geometry.setAttribute(
         "position",
@@ -65,11 +70,36 @@ class Sampler extends THREE.Mesh {
       return { scene, material };
     })();
 
-    const resolution = 256;
+    const resolution = TEXTURE_ALTAS_SIZE * SAMPLES_PER_AXIS;
     const renderTarget = new THREE.WebGLRenderTarget(resolution, resolution);
-    const geometry = new THREE.PlaneGeometry(10, 10);
-    const material = new THREE.MeshBasicMaterial({ map: renderTarget.texture });
+    const geometry = new THREE.BufferGeometry();
 
+    const vertices = ((size, scale, offset) =>
+      new Array(size[1])
+        .fill(0)
+        .flatMap((_, y) =>
+          new Array(size[2] * size[0])
+            .fill(0)
+            .map((_, index) => [
+              index % size[0],
+              y,
+              Math.floor(index / size[0]),
+            ])
+        )
+        .map((data) => data.map((value, index) => value / size[index]))
+        .map((data) =>
+          data.map((v, index) => v * scale[index] + offset[index])
+        ))(
+      [SAMPLES_PER_AXIS, LAYER_COUNT, SAMPLES_PER_AXIS],
+      SAMPLES_SCALE,
+      SAMPLES_OFFSET
+    );
+    geometry.setAttribute(
+      "position",
+      new THREE.BufferAttribute(new Float32Array(vertices.flat()), 3)
+    );
+    const material = new THREE.PointsMaterial({ vertexColors: true });
+    material.size = 0.1;
     super(geometry, material);
 
     this.heatmapPlanes = heatmapPlanes;
@@ -83,16 +113,31 @@ class Sampler extends THREE.Mesh {
     renderer.setRenderTarget(this.renderTarget);
     renderer.render(this.heatmapPlanes.scene, new THREE.Camera());
     renderer.setRenderTarget(null);
-    const read = new Uint8Array(4 * this.resolution * this.resolution);
-    renderer.readRenderTargetPixels(
-      this.renderTarget,
-      0,
-      0,
-      this.resolution,
-      this.resolution,
-      read
+    const colors = new Array(TEXTURE_ALTAS_SIZE ** 2)
+      .fill(0)
+      .reduce((prev, _, index) => {
+        const pixels = new Uint8Array(4 * SAMPLES_PER_AXIS * SAMPLES_PER_AXIS);
+        const xOffset = (index % TEXTURE_ALTAS_SIZE) * SAMPLES_PER_AXIS;
+        const yOffset =
+          Math.floor(index / TEXTURE_ALTAS_SIZE) * SAMPLES_PER_AXIS;
+        renderer.readRenderTargetPixels(
+          this.renderTarget,
+          xOffset,
+          yOffset,
+          SAMPLES_PER_AXIS,
+          SAMPLES_PER_AXIS,
+          pixels
+        );
+
+        return [...prev, ...pixels];
+      }, []);
+    this.geometry.setAttribute(
+      "color",
+      new THREE.BufferAttribute(
+        new Float32Array(colors.map((color) => color / 255)),
+        4
+      )
     );
-    console.log(read);
   }
 
   setUniforms(data) {
